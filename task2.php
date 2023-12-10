0e7b91d340e66d2c188154b0d75c3c518d8c362d
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2023-12-10 14:41:46 +0300

    Update task2.php
    Прописаны уязвимости CWE

--- task2.php
+++ task2.php
@@ -1,13 +1,25 @@
 <?php
 if( isset( $_GET[ 'Login' ] ) ) {
+    // CWE-20: Improper Input Validation
         // Get username
         $user = $_GET[ 'username' ];
         // Get password
         $pass = $_GET[ 'password' ];
+        // CWE-326: Inadequate Encryption Strength
+        // CWE-327: Use of a Broken or Risky Cryptographic Algorithm
+	    // CWE-328: Use of Weak Hash
         $pass = md5( $pass );
+        // CWE-257: Storing Passwords in a Recoverable Format
+        // CWE-89 : Improper Neutralization of Special Elements used in an SQL Command ('SQL Injection')
+        // CWE-306: Missing Authentication for Critical Function
+        // CWE-307: Improper Restriction of Excessive Authentication Attempts
+        // CWE-799: Improper Control of Interaction Frequency
         // Check the database
         $query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
+        // CWE-312: Cleartext Storage of Sensitive Information
+        // CWE-319: Cleartext Transmission of Sensitive Information
         $result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+        // CWE-209: Information Exposure Through an Error Message
         if( $result && mysqli_num_rows( $result ) == 1 ) {
                 // Get users details
                 $row    = mysqli_fetch_assoc( $result );
